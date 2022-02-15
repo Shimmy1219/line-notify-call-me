3ca215e6603fa2c10c53e820ee36c15a474202a0
shimmy <hiromu19991219@gmail.com>
2022-02-16 04:47:37 +0900

    3度目の正直

--- main.py
+++ main.py
@@ -17,15 +17,12 @@ import os
 app = Flask(__name__)
 
 # 環境変数からLINE Access Tokenを設定
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+#LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
 # 環境変数からLINE Channel Secretを設定
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+#LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 
-print(LINE_CHANNEL_ACCESS_TOKEN)
-print(LINE_CHANNEL_SECRET)
-
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(LINE_CHANNEL_SECRET)
+line_bot_api = LineBotApi("1jErePBInNYXbymrDEJ + t + 0ZD9sMhvNh15ttqGe1LMarJZOsiqhmrvMwJe7fHU6orXf + eMN5gXdFfwnTjsdMsChCBsMAeHbiNwfqEZZOBk7jHKdLewZSKvOZ6Teo")
+handler = WebhookHandler("8af8b95a1b58eab6bf0184f85acf1e2f")
 
 
 @app.route("/")
