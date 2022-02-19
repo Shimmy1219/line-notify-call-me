384168ea11eeb579e6343e2f1214f6e05c3d4a35
shimmy <hiromu19991219@gmail.com>
2022-02-19 19:26:35 +0900

    master

--- twitter.py
+++ twitter.py
@@ -13,49 +13,50 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
 cur = conn.cursor()
 
-def is_exists(cur,column_name,data):
+def is_exists(column_name,data): #コラムにデータがあるか確認する。あればTrueを返す
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
     cur.execute(
         "SELECT EXISTS (SELECT * FROM database WHERE %s = %s)",(column_name,str(data)))
     print(cur.fetchone())
-    return cur.fetchone()
+    return cur.fetchone() #return True or False
 
-def authorize_url():
+def authorize_url():  #Twitterの認証URLを返す
   try:
     redirect_url = auth.get_authorization_url()
-    return redirect_url
+    return redirect_url #return URL
   except tweepy.TweepError:
     return "Return authorization url is failed."
 
-def authentication_final(user_verifier,userid):
+def authentication_final(user_verifier,userid): #Twitterの認証をしてユーザーをデータベースに登録
   global auth,api
   session =   {
       "request_token": auth.request_token,
   }
 
-
-  # verifier = user_verifier
-  # Let's say this is a web app, so we need to re-build the auth handler
-  # first...
   auth = tweepy.OAuthHandler(CK, CS)
   token = session["request_token"]
   session.pop("request_token")
   auth.request_token = token
 
+  conn = psycopg2.connect(DATABASE_URL)
+  cur = conn.cursor()
+
 
   try:
-    ts = auth.get_access_token(user_verifier)
+    ts = auth.get_access_token(user_verifier) #TWitterのアクセストークンを取得
     token = ts[0]
     secret = ts[1]
     auth.set_access_token(token, secret)
     api = tweepy.API(auth)
     #api.update_status('test tweet!!!!!') # 認証が成功した時にツイートで確認したい方は使ってください
-    try:
+    try: #認証が有効か確認。twitterのユーザーデータを返す
       user = api.verify_credentials()
     except:
       return "The user credentials are invalid."
 
-    if is_exists(cur,'twitterid',user.id_str) == False:
-      try:
+    if is_exists(cur,'twitterid',user.id_str) == False: #もしデータベースにユーザーが存在しなかったら
+      try: #データベースにユーザーを登録
         cur.execute(
         "INSERT INTO database (\
           userid, access_token, acess_token_secret, twitterid, screen_name, name\
@@ -68,8 +69,9 @@ def authentication_final(user_verifier,userid):
         return '認証成功\nこんにちは！{}さん\nメニューからキーワードを登録することができます。'.format(user.name)
       except:
         return 'Error! Failed to access the database.'
-    else:
+    else: #もし既にユーザーがデータベースにあったら
       conn.commit()
+
       cur.close()
       conn.close()
       return '{}はログインされています'.format(user.screen_name)
