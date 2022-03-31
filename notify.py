e8cd56fcdda26ade7c6d97389374be59255f448c
shimmy <hiromu19991219@gmail.com>
2022-04-01 01:23:37 +0900

    4.1の更新

--- notify.py
+++ notify.py
@@ -1,3 +1,4 @@
+from aiohttp import Payload
 import tweepy
 from pprint import pprint
 import time
@@ -6,6 +7,8 @@ from threading import Thread
 from linebot import LineBotApi
 from linebot.models import TextSendMessage,Sender
 import datetime
+import json
+import os
 
 CK = 'ctiRJV5FXu2uBm9rW3ltLe0z2'
 CS = 'Gf2okVdVay66UO8MkQ18sTKTfvNMadJRuG5did1i5mRfLibHdw'
@@ -18,7 +21,8 @@ conn = psycopg2.connect(DATABASE_URL)
 cur = conn.cursor()
 
 def push_message(userid,text,name,url):
-    LINE_CHANNEL_ACCESS_TOKEN = 'sEsX2yDRvj454rjlxv8JQ4coXHyUTaAwOextJx0YvtkGh2eZUPwz6opSHz3HKj7erXf+eMN5gXdFfwnTjsdMsChCBsMAeHbiNwfqEZZOBk5OSLlSLHeZVoFl5CXvyM0KKslYItAO/c9kwdpKvKQcegdB04t89/1O/w1cDnyilFU='
+    #LINE_CHANNEL_ACCESS_TOKEN = 'sEsX2yDRvj454rjlxv8JQ4coXHyUTaAwOextJx0YvtkGh2eZUPwz6opSHz3HKj7erXf#+eMN5gXdFfwnTjsdMsChCBsMAeHbiNwfqEZZOBk5OSLlSLHeZVoFl5CXvyM0KKslYItAO/c9kwdpKvKQcegdB04t89/1O/w1cDnyilFU='
+    LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
     USER_ID = userid
 
     line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
@@ -36,22 +40,23 @@ class getTweet():
     self.do(lim,offset)
 
   def do(e,lim,offset):
-    cur.execute("SELECT * FROM database ORDER BY id LIMIT {} OFFSET {} ".format(str(lim),str(offset)))
-    database_list = [s for s in cur.fetchall() if s[7] != None]
+    cur.execute("SELECT * FROM database ORDER BY id LIMIT {} OFFSET {} ".format(str(lim),str(offset))) #n番目からi人のIDを取得する
+    database_list = [s for s in cur.fetchall() if s[7] != None]#もしキーワード登録欄が空欄ではなかったら、userリストに入れる
 
-    for record in database_list:
+    for record in database_list:#ユーザーリストからレコードを取り出す
       AT = record[2]
       AS = record[3]
       auth.set_access_token(AT, AS)
       api = tweepy.API(auth)
 
-      statuses = api.home_timeline(count=20)
+      statuses = api.home_timeline(count=20)#最新のタイムラインを20個取得する
       x = 0
-      for status in statuses:
-        last_time = record[8]
-        if record[8] == None:
-          last_time = datetime.datetime(2022,2,21,17,22,00)
-        if (status.created_at - last_time).total_seconds() > 0:
+      for status in statuses:#statusに取得したタイムライン20個を入れる
+        last_time = record[8]#前回取得したツイートとしてlast_tweet_create_atに保存する
+        if record[8] == None:#もし初回の取得なら
+          last_time = datetime.datetime(2022,2,21,17,22,00)#前回のツイートとして登録する
+        if (status.created_at - last_time).total_seconds() > 0:#もし今回取得したツイートから前回取得したツイートより最新だったら、
+        #前回取得したものと同じ時間なら実行されない。
         #見映えのため区切る
           print('-------------------------------------------')
           print(record[6])
@@ -59,25 +64,26 @@ class getTweet():
           print('name:' + status.user.name)
           #内容表示
           print(status.text)
-          if record[4] != status.user.id_str:
-            for keyword in record[7]:
-              if keyword in status.text:
-                push_message(record[1],status.text,status.user.name,status.user.profile_image_url_https)
+          if record[4] != status.user.id_str:#もし取得したツイートが自分のものではなかったら
+            for keyword in record[7]:#キーワードリストからキーワードを取得
+              if keyword in status.text:#もしキーワードリストのキーワードがツイートに含まれていたら
+                push_message(record[1],status.text,status.user.name,status.user.profile_image_url_https)#メッセージを送信する
+                print("Lineにメッセージを送信しました！相手＝"+str(record[1])+"キーワード＝"+str(keyword))
                 break #１回反応したらbreakする
-          if x == 0:
+          if x == 0:#もし取得した中で最新のツイートならば
             cur.execute("UPDATE database SET last_tweet_created_at = '{}'  WHERE id = '{}'".format(status.created_at,record[0]))
-            conn.commit()
-          x+=1
+            conn.commit() #データベースのlast_tweet_create_atに登録
+          x+=1#Xを増やす
 
 def run_thread():
-  STARTTIME = time.time()
-  cur.execute("SELECT id FROM database")
-  num = len(cur.fetchall())
-  Thread_num = num // 250 + 1
-  interval = num // Thread_num + 1
-  for i in range(Thread_num):
-    arg = i * interval
-    job = Thread(target=getTweet, args=(interval,arg))
+  STARTTIME = time.time() #開始時間
+  cur.execute("SELECT id FROM database")#データベースから全ユーザーのIDリストを取得
+  num = len(cur.fetchall())#何人いるかを変数に
+  Thread_num = num // 250 + 1#必要なスレッドの数を取得
+  interval = num // Thread_num + 1#一つのスレッドに何人分を取得するか
+  for i in range(Thread_num):#必要なスレッドの数だけ繰り返す
+    arg = i * interval#何番目の人を取得するか
+    job = Thread(target=getTweet, args=(interval,arg))#何人分、何番目の情報を付けてスレッド化
     job.start()
-  ENDTIME = time.time()
-  print(ENDTIME-STARTTIME)
+  ENDTIME = time.time()#終わりの時間
+  print(ENDTIME-STARTTIME)#終わりの時間から開始時間を引く。１分以内なら正常
